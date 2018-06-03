26b409f0e06346672548fc5a6519b7b9060ab5d7
西影行 <richardchen0954@gmail.com>
2018-06-04 07:44:54 +0800

    next 7-6

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :groups
   has_many :posts
-  devise :database_authenticatable, :registerable,
+
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
+devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+
 end
